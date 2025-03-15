fceb0d361c91ad929de3792b60fbbb47842c19e0
Khải Nguyễn Quang <23520676@ms.uit.edu.vn>
2025-03-15 22:19:37 +0700

    forGot_passWord_control / signUp_control Design

---
+++ sign_Up_control/sign_Up_control.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace sign_Up_control
+{
+    public partial class sign_Up_control: UserControl
+    {
+        public sign_Up_control()
+        {
+            InitializeComponent();
+        }
+    }
+}
