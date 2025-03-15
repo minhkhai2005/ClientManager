995bdb45611673b7b6f966ca832a0fb14e3de416
Tran Minh Khai <23520679@gm.uit.edu.vn>
2025-03-16 00:05:41 +0700

    design sidebar control

---
+++ SidebarControl/sidebar.Designer.cs
@@ -0,0 +1,198 @@
+﻿namespace SidebarControl
+{
+    partial class sidebar
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(sidebar));
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.dashboardBtn = new System.Windows.Forms.Button();
+            this.storesBtn = new System.Windows.Forms.Button();
+            this.employeesBtn = new System.Windows.Forms.Button();
+            this.customerBtn = new System.Windows.Forms.Button();
+            this.notificationsBtn = new System.Windows.Forms.Button();
+            this.configurationBtn = new System.Windows.Forms.Button();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.BackColor = System.Drawing.Color.White;
+            this.tableLayoutPanel1.ColumnCount = 1;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.Controls.Add(this.configurationBtn, 0, 5);
+            this.tableLayoutPanel1.Controls.Add(this.notificationsBtn, 0, 4);
+            this.tableLayoutPanel1.Controls.Add(this.customerBtn, 0, 3);
+            this.tableLayoutPanel1.Controls.Add(this.employeesBtn, 0, 2);
+            this.tableLayoutPanel1.Controls.Add(this.storesBtn, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.dashboardBtn, 0, 0);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Margin = new System.Windows.Forms.Padding(0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 7;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 80F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 80F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 80F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 80F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 80F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 80F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(420, 880);
+            this.tableLayoutPanel1.TabIndex = 0;
+            // 
+            // dashboardBtn
+            // 
+            this.dashboardBtn.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dashboardBtn.FlatAppearance.BorderSize = 0;
+            this.dashboardBtn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.dashboardBtn.Font = new System.Drawing.Font("Afacad", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.dashboardBtn.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(87)))), ((int)(((byte)(85)))), ((int)(((byte)(85)))));
+            this.dashboardBtn.Image = ((System.Drawing.Image)(resources.GetObject("dashboardBtn.Image")));
+            this.dashboardBtn.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.dashboardBtn.Location = new System.Drawing.Point(0, 0);
+            this.dashboardBtn.Margin = new System.Windows.Forms.Padding(0);
+            this.dashboardBtn.Name = "dashboardBtn";
+            this.dashboardBtn.Padding = new System.Windows.Forms.Padding(20, 0, 0, 0);
+            this.dashboardBtn.Size = new System.Drawing.Size(420, 80);
+            this.dashboardBtn.TabIndex = 0;
+            this.dashboardBtn.Text = "Dashboard";
+            this.dashboardBtn.UseVisualStyleBackColor = true;
+            // 
+            // storesBtn
+            // 
+            this.storesBtn.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.storesBtn.FlatAppearance.BorderSize = 0;
+            this.storesBtn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.storesBtn.Font = new System.Drawing.Font("Afacad", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.storesBtn.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(87)))), ((int)(((byte)(85)))), ((int)(((byte)(85)))));
+            this.storesBtn.Image = ((System.Drawing.Image)(resources.GetObject("storesBtn.Image")));
+            this.storesBtn.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.storesBtn.Location = new System.Drawing.Point(0, 80);
+            this.storesBtn.Margin = new System.Windows.Forms.Padding(0);
+            this.storesBtn.Name = "storesBtn";
+            this.storesBtn.Padding = new System.Windows.Forms.Padding(20, 0, 0, 0);
+            this.storesBtn.Size = new System.Drawing.Size(420, 80);
+            this.storesBtn.TabIndex = 1;
+            this.storesBtn.Text = "Stores";
+            this.storesBtn.UseVisualStyleBackColor = true;
+            // 
+            // employeesBtn
+            // 
+            this.employeesBtn.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.employeesBtn.FlatAppearance.BorderSize = 0;
+            this.employeesBtn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.employeesBtn.Font = new System.Drawing.Font("Afacad", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.employeesBtn.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(87)))), ((int)(((byte)(85)))), ((int)(((byte)(85)))));
+            this.employeesBtn.Image = ((System.Drawing.Image)(resources.GetObject("employeesBtn.Image")));
+            this.employeesBtn.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.employeesBtn.Location = new System.Drawing.Point(0, 160);
+            this.employeesBtn.Margin = new System.Windows.Forms.Padding(0);
+            this.employeesBtn.Name = "employeesBtn";
+            this.employeesBtn.Padding = new System.Windows.Forms.Padding(20, 0, 0, 0);
+            this.employeesBtn.Size = new System.Drawing.Size(420, 80);
+            this.employeesBtn.TabIndex = 2;
+            this.employeesBtn.Text = "Employees";
+            this.employeesBtn.UseVisualStyleBackColor = true;
+            // 
+            // customerBtn
+            // 
+            this.customerBtn.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.customerBtn.FlatAppearance.BorderSize = 0;
+            this.customerBtn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.customerBtn.Font = new System.Drawing.Font("Afacad", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.customerBtn.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(87)))), ((int)(((byte)(85)))), ((int)(((byte)(85)))));
+            this.customerBtn.Image = ((System.Drawing.Image)(resources.GetObject("customerBtn.Image")));
+            this.customerBtn.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.customerBtn.Location = new System.Drawing.Point(0, 240);
+            this.customerBtn.Margin = new System.Windows.Forms.Padding(0);
+            this.customerBtn.Name = "customerBtn";
+            this.customerBtn.Padding = new System.Windows.Forms.Padding(20, 0, 0, 0);
+            this.customerBtn.Size = new System.Drawing.Size(420, 80);
+            this.customerBtn.TabIndex = 3;
+            this.customerBtn.Text = "Customers";
+            this.customerBtn.UseVisualStyleBackColor = true;
+            // 
+            // notificationsBtn
+            // 
+            this.notificationsBtn.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.notificationsBtn.FlatAppearance.BorderSize = 0;
+            this.notificationsBtn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.notificationsBtn.Font = new System.Drawing.Font("Afacad", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.notificationsBtn.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(87)))), ((int)(((byte)(85)))), ((int)(((byte)(85)))));
+            this.notificationsBtn.Image = ((System.Drawing.Image)(resources.GetObject("notificationsBtn.Image")));
+            this.notificationsBtn.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.notificationsBtn.Location = new System.Drawing.Point(0, 320);
+            this.notificationsBtn.Margin = new System.Windows.Forms.Padding(0);
+            this.notificationsBtn.Name = "notificationsBtn";
+            this.notificationsBtn.Padding = new System.Windows.Forms.Padding(20, 0, 0, 0);
+            this.notificationsBtn.Size = new System.Drawing.Size(420, 80);
+            this.notificationsBtn.TabIndex = 4;
+            this.notificationsBtn.Text = "Notifications";
+            this.notificationsBtn.UseVisualStyleBackColor = true;
+            // 
+            // configurationBtn
+            // 
+            this.configurationBtn.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.configurationBtn.FlatAppearance.BorderSize = 0;
+            this.configurationBtn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.configurationBtn.Font = new System.Drawing.Font("Afacad", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.configurationBtn.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(87)))), ((int)(((byte)(85)))), ((int)(((byte)(85)))));
+            this.configurationBtn.Image = ((System.Drawing.Image)(resources.GetObject("configurationBtn.Image")));
+            this.configurationBtn.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.configurationBtn.Location = new System.Drawing.Point(0, 400);
+            this.configurationBtn.Margin = new System.Windows.Forms.Padding(0);
+            this.configurationBtn.Name = "configurationBtn";
+            this.configurationBtn.Padding = new System.Windows.Forms.Padding(20, 0, 0, 0);
+            this.configurationBtn.Size = new System.Drawing.Size(420, 80);
+            this.configurationBtn.TabIndex = 5;
+            this.configurationBtn.Text = "Configuration";
+            this.configurationBtn.UseVisualStyleBackColor = true;
+            // 
+            // sidebar
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Name = "sidebar";
+            this.Size = new System.Drawing.Size(420, 880);
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.Button configurationBtn;
+        private System.Windows.Forms.Button notificationsBtn;
+        private System.Windows.Forms.Button customerBtn;
+        private System.Windows.Forms.Button employeesBtn;
+        private System.Windows.Forms.Button storesBtn;
+        private System.Windows.Forms.Button dashboardBtn;
+    }
+}
---
+++ SidebarControl/sidebar.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SidebarControl
+{
+    public partial class sidebar: UserControl
+    {
+        public sidebar()
+        {
+            InitializeComponent();
+            dashboardBtn.Click += Button_Click;
+            storesBtn.Click += Button_Click;
+            employeesBtn.Click += Button_Click;
+            customerBtn.Click += Button_Click;
+            notificationsBtn.Click += Button_Click;
+            configurationBtn.Click += Button_Click;
+        }
+
+        private void Button_Click(object sender, EventArgs e)
+        {
+            // Change the BackColor of all buttons to white
+            foreach (Control control in tableLayoutPanel1.Controls)
+            {
+                if (control is Button button)
+                {
+                    button.BackColor = Color.White;
+                    button.ForeColor = Color.FromArgb(87,85,85); // Optionally reset the ForeColor as well
+                }
+            }
+            Button btn = sender as Button;
+            if (btn != null)
+            {
+                btn.BackColor = Color.FromArgb(41, 128, 185);
+                btn.ForeColor = Color.FromArgb(255, 255, 255);
+            }
+
+        }
+
+
+    }
+}
