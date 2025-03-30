9f7be4b2ddfcc22770f930ce0e0abb9d8817cf92
Tran Minh Khai <23520679@gm.uit.edu.vn>
2025-03-30 14:54:40 +0700

    design dashboard page

---
+++ Dashboard/Dashboard.Designer.cs
@@ -0,0 +1,230 @@
+﻿namespace Dashboard
+{
+    partial class Dashboard
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.tableLayoutPanel2 = new System.Windows.Forms.TableLayoutPanel();
+            this.TimeBox = new System.Windows.Forms.ComboBox();
+            this.formsPlot1 = new ScottPlot.WinForms.FormsPlot();
+            this.tableLayoutPanel3 = new System.Windows.Forms.TableLayoutPanel();
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.flowLayoutPanel2 = new System.Windows.Forms.FlowLayoutPanel();
+            this.RevenueText = new System.Windows.Forms.Label();
+            this.RevenueValue = new System.Windows.Forms.Label();
+            this.OrderText = new System.Windows.Forms.Label();
+            this.OrderValue = new System.Windows.Forms.Label();
+            this.Title = new System.Windows.Forms.Label();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.tableLayoutPanel2.SuspendLayout();
+            this.tableLayoutPanel3.SuspendLayout();
+            this.flowLayoutPanel1.SuspendLayout();
+            this.flowLayoutPanel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 1;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.Controls.Add(this.tableLayoutPanel2, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.tableLayoutPanel3, 0, 0);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 2;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 36.67426F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 63.32574F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(951, 878);
+            this.tableLayoutPanel1.TabIndex = 0;
+            // 
+            // tableLayoutPanel2
+            // 
+            this.tableLayoutPanel2.ColumnCount = 1;
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel2.Controls.Add(this.TimeBox, 0, 0);
+            this.tableLayoutPanel2.Controls.Add(this.formsPlot1, 0, 1);
+            this.tableLayoutPanel2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel2.Location = new System.Drawing.Point(3, 325);
+            this.tableLayoutPanel2.Name = "tableLayoutPanel2";
+            this.tableLayoutPanel2.RowCount = 2;
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 5.62249F));
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 94.37751F));
+            this.tableLayoutPanel2.Size = new System.Drawing.Size(945, 550);
+            this.tableLayoutPanel2.TabIndex = 0;
+            // 
+            // TimeBox
+            // 
+            this.TimeBox.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.TimeBox.FormattingEnabled = true;
+            this.TimeBox.Location = new System.Drawing.Point(3, 3);
+            this.TimeBox.Name = "TimeBox";
+            this.TimeBox.Size = new System.Drawing.Size(121, 21);
+            this.TimeBox.TabIndex = 0;
+            // 
+            // formsPlot1
+            // 
+            this.formsPlot1.DisplayScale = 0F;
+            this.formsPlot1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.formsPlot1.Location = new System.Drawing.Point(3, 33);
+            this.formsPlot1.Name = "formsPlot1";
+            this.formsPlot1.Size = new System.Drawing.Size(939, 514);
+            this.formsPlot1.TabIndex = 1;
+            // 
+            // tableLayoutPanel3
+            // 
+            this.tableLayoutPanel3.ColumnCount = 1;
+            this.tableLayoutPanel3.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel3.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tableLayoutPanel3.Controls.Add(this.flowLayoutPanel1, 0, 1);
+            this.tableLayoutPanel3.Controls.Add(this.flowLayoutPanel2, 0, 2);
+            this.tableLayoutPanel3.Controls.Add(this.Title, 0, 0);
+            this.tableLayoutPanel3.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel3.Location = new System.Drawing.Point(3, 3);
+            this.tableLayoutPanel3.Name = "tableLayoutPanel3";
+            this.tableLayoutPanel3.RowCount = 3;
+            this.tableLayoutPanel3.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 44F));
+            this.tableLayoutPanel3.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 56F));
+            this.tableLayoutPanel3.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 125F));
+            this.tableLayoutPanel3.Size = new System.Drawing.Size(945, 316);
+            this.tableLayoutPanel3.TabIndex = 1;
+            // 
+            // flowLayoutPanel1
+            // 
+            this.flowLayoutPanel1.Controls.Add(this.RevenueText);
+            this.flowLayoutPanel1.Controls.Add(this.RevenueValue);
+            this.flowLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.flowLayoutPanel1.FlowDirection = System.Windows.Forms.FlowDirection.TopDown;
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(3, 87);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(939, 100);
+            this.flowLayoutPanel1.TabIndex = 0;
+            this.flowLayoutPanel1.WrapContents = false;
+            // 
+            // flowLayoutPanel2
+            // 
+            this.flowLayoutPanel2.Controls.Add(this.OrderText);
+            this.flowLayoutPanel2.Controls.Add(this.OrderValue);
+            this.flowLayoutPanel2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.flowLayoutPanel2.FlowDirection = System.Windows.Forms.FlowDirection.TopDown;
+            this.flowLayoutPanel2.Location = new System.Drawing.Point(3, 193);
+            this.flowLayoutPanel2.Name = "flowLayoutPanel2";
+            this.flowLayoutPanel2.Size = new System.Drawing.Size(939, 120);
+            this.flowLayoutPanel2.TabIndex = 1;
+            this.flowLayoutPanel2.WrapContents = false;
+            // 
+            // RevenueText
+            // 
+            this.RevenueText.AutoSize = true;
+            this.RevenueText.Font = new System.Drawing.Font("Afacad", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.RevenueText.Location = new System.Drawing.Point(3, 0);
+            this.RevenueText.Name = "RevenueText";
+            this.RevenueText.Size = new System.Drawing.Size(128, 43);
+            this.RevenueText.TabIndex = 0;
+            this.RevenueText.Text = "Revenue";
+            this.RevenueText.Click += new System.EventHandler(this.RevenueText_Click);
+            // 
+            // RevenueValue
+            // 
+            this.RevenueValue.AutoSize = true;
+            this.RevenueValue.Font = new System.Drawing.Font("Afacad", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.RevenueValue.Location = new System.Drawing.Point(3, 43);
+            this.RevenueValue.Name = "RevenueValue";
+            this.RevenueValue.Size = new System.Drawing.Size(144, 43);
+            this.RevenueValue.TabIndex = 1;
+            this.RevenueValue.Text = "$130,000";
+            // 
+            // OrderText
+            // 
+            this.OrderText.AutoSize = true;
+            this.OrderText.Font = new System.Drawing.Font("Afacad", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.OrderText.Location = new System.Drawing.Point(3, 0);
+            this.OrderText.Name = "OrderText";
+            this.OrderText.Size = new System.Drawing.Size(107, 43);
+            this.OrderText.TabIndex = 1;
+            this.OrderText.Text = "Orders";
+            // 
+            // OrderValue
+            // 
+            this.OrderValue.AutoSize = true;
+            this.OrderValue.Font = new System.Drawing.Font("Afacad", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.OrderValue.Location = new System.Drawing.Point(3, 43);
+            this.OrderValue.Name = "OrderValue";
+            this.OrderValue.Size = new System.Drawing.Size(110, 43);
+            this.OrderValue.TabIndex = 2;
+            this.OrderValue.Text = "12,380";
+            // 
+            // Title
+            // 
+            this.Title.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
+            this.Title.AutoSize = true;
+            this.Title.Font = new System.Drawing.Font("Afacad SemiBold", 32F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Title.Location = new System.Drawing.Point(3, 0);
+            this.Title.Name = "Title";
+            this.Title.Size = new System.Drawing.Size(213, 84);
+            this.Title.TabIndex = 2;
+            this.Title.Text = "Dashboard";
+            this.Title.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // Dashboard
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Name = "Dashboard";
+            this.Size = new System.Drawing.Size(951, 878);
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel2.ResumeLayout(false);
+            this.tableLayoutPanel3.ResumeLayout(false);
+            this.tableLayoutPanel3.PerformLayout();
+            this.flowLayoutPanel1.ResumeLayout(false);
+            this.flowLayoutPanel1.PerformLayout();
+            this.flowLayoutPanel2.ResumeLayout(false);
+            this.flowLayoutPanel2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel2;
+        private System.Windows.Forms.ComboBox TimeBox;
+        private ScottPlot.WinForms.FormsPlot formsPlot1;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel3;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+        private System.Windows.Forms.Label RevenueText;
+        private System.Windows.Forms.Label RevenueValue;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel2;
+        private System.Windows.Forms.Label OrderText;
+        private System.Windows.Forms.Label OrderValue;
+        private System.Windows.Forms.Label Title;
+    }
+}
---
+++ Dashboard/Dashboard.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Dashboard
+{
+   
+    public partial class Dashboard: UserControl
+    {
+        readonly List<string> TimeBoxItems = new List<String> { "Month", "Week", "Day" };
+
+        public Dashboard()
+        {
+            InitializeComponent();
+            TimeBox.DataSource = TimeBoxItems;
+        }
+
+        private void RevenueText_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
