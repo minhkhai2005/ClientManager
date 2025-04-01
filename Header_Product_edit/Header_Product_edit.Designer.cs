9547bdecbe0989b8355299ca697e91b1cd29343b
Khải Nguyễn Quang <23520676@ms.uit.edu.vn>
2025-04-01 22:30:08 +0700

    Header_Product_edit

---
+++ Header_Product_edit/Header_Product_edit.Designer.cs
@@ -0,0 +1,226 @@
+﻿namespace Header_Product_edit
+{
+    partial class Header_Product_edit
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.ID_TXT = new System.Windows.Forms.Label();
+            this.ST_TXT = new System.Windows.Forms.Label();
+            this.SOLD_TXT = new System.Windows.Forms.Label();
+            this.Price_TXT = new System.Windows.Forms.Label();
+            this.Revenue_TXT = new System.Windows.Forms.Label();
+            this.panelStatus = new System.Windows.Forms.Panel();
+            this.lblStatus = new System.Windows.Forms.Label();
+            this.btnToggleStatus = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(17, 14);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(100, 100);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("Afacad", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(646, 14);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(82, 32);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Revenue";
+            // 
+            // label2
+            // 
+            this.label2.Font = new System.Drawing.Font("Afacad", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(517, 14);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(47, 34);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Price";
+            // 
+            // label3
+            // 
+            this.label3.Font = new System.Drawing.Font("Afacad", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(413, 16);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(41, 32);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Sold";
+            // 
+            // label4
+            // 
+            this.label4.Font = new System.Drawing.Font("Afacad", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(326, 16);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(52, 32);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "Stock";
+            // 
+            // label5
+            // 
+            this.label5.Font = new System.Drawing.Font("Afacad", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(126, 15);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(146, 32);
+            this.label5.TabIndex = 5;
+            this.label5.Text = "Product’s name";
+            // 
+            // ID_TXT
+            // 
+            this.ID_TXT.AutoSize = true;
+            this.ID_TXT.Font = new System.Drawing.Font("Afacad", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ID_TXT.Location = new System.Drawing.Point(129, 42);
+            this.ID_TXT.Name = "ID_TXT";
+            this.ID_TXT.Size = new System.Drawing.Size(72, 23);
+            this.ID_TXT.TabIndex = 6;
+            this.ID_TXT.Text = "ID: 1234";
+            // 
+            // ST_TXT
+            // 
+            this.ST_TXT.AutoSize = true;
+            this.ST_TXT.Font = new System.Drawing.Font("Afacad", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ST_TXT.Location = new System.Drawing.Point(326, 42);
+            this.ST_TXT.Name = "ST_TXT";
+            this.ST_TXT.Size = new System.Drawing.Size(37, 23);
+            this.ST_TXT.TabIndex = 7;
+            this.ST_TXT.Text = "900";
+            // 
+            // SOLD_TXT
+            // 
+            this.SOLD_TXT.AutoSize = true;
+            this.SOLD_TXT.Font = new System.Drawing.Font("Afacad", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.SOLD_TXT.Location = new System.Drawing.Point(413, 42);
+            this.SOLD_TXT.Name = "SOLD_TXT";
+            this.SOLD_TXT.Size = new System.Drawing.Size(37, 23);
+            this.SOLD_TXT.TabIndex = 8;
+            this.SOLD_TXT.Text = "102";
+            // 
+            // Price_TXT
+            // 
+            this.Price_TXT.AutoSize = true;
+            this.Price_TXT.Location = new System.Drawing.Point(517, 42);
+            this.Price_TXT.Name = "Price_TXT";
+            this.Price_TXT.Size = new System.Drawing.Size(42, 16);
+            this.Price_TXT.TabIndex = 9;
+            this.Price_TXT.Text = "$1200";
+            // 
+            // Revenue_TXT
+            // 
+            this.Revenue_TXT.AutoSize = true;
+            this.Revenue_TXT.Font = new System.Drawing.Font("Afacad", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Revenue_TXT.Location = new System.Drawing.Point(646, 42);
+            this.Revenue_TXT.Name = "Revenue_TXT";
+            this.Revenue_TXT.Size = new System.Drawing.Size(68, 23);
+            this.Revenue_TXT.TabIndex = 10;
+            this.Revenue_TXT.Text = "$122.4K";
+            // 
+            // panelStatus
+            // 
+            this.panelStatus.BackColor = System.Drawing.Color.Lime;
+            this.panelStatus.Location = new System.Drawing.Point(132, 77);
+            this.panelStatus.Name = "panelStatus";
+            this.panelStatus.Size = new System.Drawing.Size(20, 16);
+            this.panelStatus.TabIndex = 11;
+            this.panelStatus.Paint += new System.Windows.Forms.PaintEventHandler(this.panelStatus_Paint);
+            // 
+            // lblStatus
+            // 
+            this.lblStatus.AutoSize = true;
+            this.lblStatus.BackColor = System.Drawing.Color.Transparent;
+            this.lblStatus.ForeColor = System.Drawing.Color.Lime;
+            this.lblStatus.Location = new System.Drawing.Point(158, 77);
+            this.lblStatus.Name = "lblStatus";
+            this.lblStatus.Size = new System.Drawing.Size(44, 16);
+            this.lblStatus.TabIndex = 12;
+            this.lblStatus.Text = "Active";
+            this.lblStatus.Click += new System.EventHandler(this.lblStatus_Click);
+            // 
+            // btnToggleStatus
+            // 
+            this.btnToggleStatus.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnToggleStatus.Location = new System.Drawing.Point(208, 72);
+            this.btnToggleStatus.Name = "btnToggleStatus";
+            this.btnToggleStatus.Size = new System.Drawing.Size(102, 27);
+            this.btnToggleStatus.TabIndex = 14;
+            this.btnToggleStatus.Text = "Toggle Status";
+            this.btnToggleStatus.UseVisualStyleBackColor = true;
+            this.btnToggleStatus.Click += new System.EventHandler(this.btnToggleStatus_Click);
+            // 
+            // UserControl1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.btnToggleStatus);
+            this.Controls.Add(this.lblStatus);
+            this.Controls.Add(this.panelStatus);
+            this.Controls.Add(this.Revenue_TXT);
+            this.Controls.Add(this.Price_TXT);
+            this.Controls.Add(this.SOLD_TXT);
+            this.Controls.Add(this.ST_TXT);
+            this.Controls.Add(this.ID_TXT);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "UserControl1";
+            this.Size = new System.Drawing.Size(780, 125);
+            this.Load += new System.EventHandler(this.UserControl1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label ID_TXT;
+        private System.Windows.Forms.Label ST_TXT;
+        private System.Windows.Forms.Label SOLD_TXT;
+        private System.Windows.Forms.Label Price_TXT;
+        private System.Windows.Forms.Label Revenue_TXT;
+        private System.Windows.Forms.Panel panelStatus;
+        private System.Windows.Forms.Label lblStatus;
+        private System.Windows.Forms.Button btnToggleStatus;
+    }
+}
---
+++ Header_Product_edit/Header_Product_edit.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Header_Product_edit
+{
+    public partial class Header_Product_edit: UserControl
+    {
+        public Header_Product_edit()
+        {
+            InitializeComponent();
+        }
+
+        private void lblStatus_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private bool isActive = true;
+
+        private void btnToggleStatus_Click(object sender, EventArgs e)
+        {
+            if (isActive)
+            {
+                panelStatus.BackColor = Color.Red;
+                lblStatus.Text = "Closed";
+                lblStatus.ForeColor = Color.Red;
+            }
+            else
+            {
+                panelStatus.BackColor = Color.Green;
+                lblStatus.Text = "Active";
+                lblStatus.ForeColor = Color.Green;
+            }
+
+            isActive = !isActive; // Đảo trạng thái
+        }
+
+        private void panelStatus_Paint(object sender, PaintEventArgs e)
+        {
+            Graphics g = e.Graphics;
+            g.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.AntiAlias;
+            Brush brush = new SolidBrush(panelStatus.BackColor);
+            g.FillEllipse(brush, 0, 0, panelStatus.Width, panelStatus.Height);
+        }
+
+        private void UserControl1_Load(object sender, EventArgs e)
+        {
+            panelStatus.Paint += new PaintEventHandler(panelStatus_Paint);
+        }
+
+    }
+}
+
