6664d12694c218147d85091921166848f9cfa868
Khải Nguyễn Quang <23520676@ms.uit.edu.vn>
2025-04-01 22:54:28 +0700

    Body_product_import

---
+++ Body_product_import/Body_product_import.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Body_product_import
+{
+    public partial class Body_product_import: UserControl
+    {
+        public Body_product_import()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void UserControl1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
