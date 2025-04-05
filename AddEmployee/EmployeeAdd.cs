7cd2e2170a849b3ed23f9593aa8329c4bc969cbd
levong1803 <kanekirito1279@gmail.com>
2025-04-05 22:12:28 +0700

    Update EmployeeAdd.cs

--- AddEmployee/EmployeeAdd.cs
+++ AddEmployee/EmployeeAdd.cs
@@ -1,1 +1,56 @@
 
+using System;
+using System.Windows.Forms;
+
+namespace EmployeeAdd
+{
+    public partial class EmployeeAdd : UserControl
+    {
+        public EmployeeForm()
+        {
+            InitializeComponent();
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            // Lấy dữ liệu từ các trường nhập liệu
+            string startDate = dtpStartDate.Value.ToShortDateString();
+            string role = txtRole.Text;
+            string salary = txtSalary.Text;
+            string phone = txtPhone.Text;
+            string shift = txtShift.Text;
+            string email = txtEmail.Text;
+            string dob = dtpDOB.Value.ToShortDateString();
+            string storeID = txtStoreID.Text;
+
+            // Hiển thị thông báo thành công
+            MessageBox.Show($"Nhân viên đã được thêm thành công!\n\nNgày bắt đầu: {startDate}\nVai trò: {role}\nLương: {salary}\nĐiện thoại: {phone}\nCa làm việc: {shift}\nEmail: {email}\nNgày sinh: {dob}\nMã cửa hàng: {storeID}",
+                "Thành công", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        private void btnCancel_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void EmployeeForm_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void employeeStartDateLabel_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void storeIdLabel_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void dtpDOB_ValueChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
