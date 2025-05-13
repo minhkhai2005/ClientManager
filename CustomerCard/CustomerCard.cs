3cf5d710f1b61080a1503ae5a13341c0f7c5947d
Lê Vọng <kanekirito1279@gmail.com>
2025-05-13 11:41:09 +0700

    Thêm thư mục/file CustomerCard

---
+++ CustomerCard/CustomerCard.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace CustomerCard
+{
+    public partial class CustomerCard : UserControl
+    {
+        public CustomerCard()
+        {
+            InitializeComponent();
+        }
+
+        // Public properties để set data từ bên ngoài
+        public string CustomerName
+        {
+            get { return labelName.Text; }
+            set { labelName.Text = value; }
+        }
+
+        public string CustomerID
+        {
+            get { return labelCustomerID.Text; }
+            set { labelCustomerID.Text = value; }
+        }
+
+        public string Phone
+        {
+            get { return labelPhone.Text; }
+            set { labelPhone.Text = value; }
+        }
+    }
+}
