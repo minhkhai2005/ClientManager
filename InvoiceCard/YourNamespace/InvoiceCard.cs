3aa4bcec0012a87bd5659855d02d103879b1ac4a
Lê Vọng <kanekirito1279@gmail.com>
2025-05-13 11:33:12 +0700

    Thêm thư mục/file Invoice__Card

---
+++ InvoiceCard/YourNamespace/InvoiceCard.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace InvoiceCard
+{
+    public partial class InvoiceCard : UserControl
+    {
+        public InvoiceCard()
+        {
+            InitializeComponent();
+        }
+
+        // Public properties để set data từ bên ngoài
+        public string InvoiceNumber
+        {
+            get { return labelInvoiceNumber.Text; }
+            set { labelInvoiceNumber.Text = value; }
+        }
+
+        public string Time
+        {
+            get { return labelTime.Text; }
+            set { labelTime.Text = value; }
+        }
+
+        public string Total
+        {
+            get { return labelTotal.Text; }
+            set { labelTotal.Text = value; }
+        }
+
+        public string NoItems
+        {
+            get { return labelNoItems.Text; }
+            set { labelNoItems.Text = value; }
+        }
+    }
+}
